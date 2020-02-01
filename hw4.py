6b129358c7b619b78e6ee658199621f8aab165f5
haasimi <59430983+haasimi@users.noreply.github.com>
2020-02-01 22:22:50 +0900

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908/',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+#nth-child(1) > td.info > a.artist.ellipsis
+#songlists = soup.select('#mArticle > div.rank_news > ul.list_news2 > li')
+# movies (tr들) 의 반복문을 돌리기
+number = 1
+for song in songs:
+    # movie 안에 a 가 있으면,
+    title = song.find("a", class_="title ellipsis").text.strip()
+    artist = song.find("a", class_="artist ellipsis").text.strip()
+    print(number,title, artist)
+
+    doc = {
+        'number': number,
+        'title': title,
+        'artist': artist
+    }
+
+    db.songs.insert_one(doc)
+    number += 1
